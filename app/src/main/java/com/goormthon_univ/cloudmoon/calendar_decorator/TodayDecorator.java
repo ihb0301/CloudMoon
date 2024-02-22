9b461349254fc356ea0d2f4c7d1cea7b25c71ea7
ihb0301 <abin0301@naver.com>
2024-02-22 20:35:52 +0900

    버그 수정: TodayDecorator에 해당하는 날은 클릭 시 색이 변하는 오류 수정

--- app/src/main/java/com/goormthon_univ/cloudmoon/calendar_decorator/TodayDecorator.java
+++ app/src/main/java/com/goormthon_univ/cloudmoon/calendar_decorator/TodayDecorator.java
@@ -1,7 +1,9 @@
 package com.goormthon_univ.cloudmoon.calendar_decorator;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.text.style.ForegroundColorSpan;
 
 import com.goormthon_univ.cloudmoon.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -28,6 +30,8 @@ public class TodayDecorator implements DayViewDecorator {
     @Override
     public void decorate(DayViewFacade view) {
         view.setSelectionDrawable(drawable);
+        //TODO TodayDecorator와 AllDecorator가 겹치는 경우 글자색 버그 있음
+        view.addSpan(new ForegroundColorSpan(Color.BLACK));
     }
 
 }
